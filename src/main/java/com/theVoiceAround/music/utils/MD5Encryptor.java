dc4f07dc81f9a92bc09e5ab9e3c2b39b4371770a
taliyah <719659083@qq.com>
2021-03-25 09:28:50 +0800

    加密工具

--- src/main/java/com/theVoiceAround/music/utils/MD5Encryptor.java
+++ src/main/java/com/theVoiceAround/music/utils/MD5Encryptor.java
@@ -12,21 +12,25 @@ import java.security.NoSuchAlgorithmException;
  * @description 加密工具
  */
 public class MD5Encryptor {
-    /**利用MD5进行加密*/
+    /**
+     * 利用MD5进行加密
+     */
     public String EncoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         //确定计算方法
-        MessageDigest md5=MessageDigest.getInstance("MD5");
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
         BASE64Encoder base64en = new BASE64Encoder();
         //加密后的字符串
-        String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
-        return newstr;
+        String strByMD5 = base64en.encode(md5.digest(str.getBytes("utf-8")));
+        return strByMD5;
     }
 
-    /**判断用户密码是否正确
-     *newpasswd 用户输入的密码
-     *oldpasswd 正确密码*/
-    public boolean checkpassword(String newpasswd,String oldpasswd) throws NoSuchAlgorithmException, UnsupportedEncodingException{
-        if(EncoderByMd5(newpasswd).equals(oldpasswd))
+    /**
+     * 判断用户密码是否正确
+     * inputPassword 用户输入的密码
+     * correctPassword 正确密码
+     */
+    public boolean checkPassword(String inputPassword,String correctPassword) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+        if(EncoderByMd5(inputPassword).equals(correctPassword))
             return true;
         else
             return false;
